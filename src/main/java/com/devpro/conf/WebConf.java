ed026eded1cdb3bc763e21ff4e026d4c9a366738
Trương Văn Tân <tantruongvan543@gmail.com>
2025-03-14 16:42:18 +0700

    first commit

---
+++ src/main/java/com/devpro/conf/WebConf.java
@@ -0,0 +1,36 @@
+package com.devpro.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class WebConf implements WebMvcConfigurer {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/META-INF/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/META-INF/js/");
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/META-INF/images/");
+		registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/META-INF/fonts/");
+		registry.addResourceHandler("/vendor/**").addResourceLocations("classpath:/META-INF/vendor/");
+		registry.addResourceHandler("/summernote/**").addResourceLocations("classpath:/META-INF/summernote/");
+		
+		// Sửa đường dẫn để trỏ tới thư mục upload thực tế của bạn
+		registry.addResourceHandler("/file/upload/**")
+				.addResourceLocations("file:///D:/ki2nam4/dambaoclpm/DoAnTotNghiepHaUI/upload/");
+	}
+}
